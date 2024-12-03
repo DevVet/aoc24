e19511aa32e26bdefa9d17b528c3b056bce4b248
Benjamin Winchester <ben.winchester@gulfstream.com>
2024-12-03 07:11:58 -0500

    minor refactor day 3

--- day/3.ts
+++ day/3.ts
@@ -7,10 +7,8 @@ export const part1 = async () => {
   const lines = await getTodaysLines("3");
   let sum = 0;
   lines.forEach((line) => {
-    const matches = line.matchAll(/mul\(\d{1,3},\d{1,3}\)/g);
-    matches.forEach((match) => {
-      const numbers = match[0].matchAll(/\d{1,3}/g);
-      sum += numbers.reduce((acc, val) => (acc *= Number(val)), 1);
+    line.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g).forEach((match) => {
+      sum += Number(match[1]) * Number(match[2]);
     });
   });
   console.log(sum);
@@ -21,16 +19,15 @@ export const part2 = async () => {
   let sum = 0;
   let isEnabled = true;
   lines.forEach((line) => {
-    const matches = line.matchAll(
-      /do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)/g
-    );
-    matches.forEach((match) => {
-      if (match[1] && match[2]) {
-        if (isEnabled) sum += Number(match[1]) * Number(match[2]);
-      } else {
-        isEnabled = match[0] === "do()";
-      }
-    });
+    line
+      .matchAll(/do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)/g)
+      .forEach((match) => {
+        if (match[1] && match[2]) {
+          if (isEnabled) sum += Number(match[1]) * Number(match[2]);
+        } else {
+          isEnabled = match[0] === "do()";
+        }
+      });
   });
   console.log(sum);
 };
