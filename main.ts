6b2c194eadb3498035a7d799f7d7395c04cd7d35
Benjamin Winchester <ben.winchester@gulfstream.com>
2024-12-03 07:04:32 -0500

    remove switch statement and dynamically import day module

--- main.ts
+++ main.ts
@@ -1,8 +1,5 @@
 import { parseArgs } from "jsr:@std/cli/parse-args";
 import "jsr:@std/dotenv/load";
-import * as day1 from "./day/1.ts";
-import * as day2 from "./day/2.ts";
-import * as day3 from "./day/3.ts";
 import { play } from "./utils.ts";
 
 if (import.meta.main) {
@@ -10,21 +7,6 @@ if (import.meta.main) {
     string: ["day", "part"],
   });
 
-  switch (flags.day) {
-    case "1":
-      play(day1, flags.part);
-      break;
-    case "2":
-      play(day2, flags.part);
-      break;
-    case "3":
-      play(day3, flags.part);
-      break;
-    default:
-      throw new Error(
-        `Must include the day to run, which must be a number between 1 - ${
-          new Date().getDay() + 1
-        }`
-      );
-  }
+  const module = await import(`./day/${flags.day}.ts`);
+  play(module, flags.part);
 }
